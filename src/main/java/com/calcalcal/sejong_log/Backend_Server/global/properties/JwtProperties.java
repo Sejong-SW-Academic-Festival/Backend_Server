eea182f29231dc5787022166a0f787c197942dc8
IdeaBank <isp_lwj@naver.com>
2023-11-24 15:06:58 +0900

    [ADD] 이것저것 추가 기능

--- src/main/java/com/calcalcal/sejong_log/Backend_Server/global/properties/JwtProperties.java
+++ src/main/java/com/calcalcal/sejong_log/Backend_Server/global/properties/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.calcalcal.sejong_log.Backend_Server.global.properties;
 
 public class JwtProperties {
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; // 30분
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 30분
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 1주일
     public static final String JWT_ACCESS_TOKEN_HEADER_NAME = "Authorization";
     public static final String JWT_ACCESS_TOKEN_TYPE = "Bearer ";
