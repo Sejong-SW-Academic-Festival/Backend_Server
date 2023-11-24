f6d1d4732c3891137ccdad464bf2812a5b72f1a3
BOK-MINJEONG <129048011+BOK-MINJEONG@users.noreply.github.com>
2023-11-24 15:44:36 +0900

    [ADD] enum, data 추가
    PERSONAL 추가
    NONE 삭제

--- src/main/java/com/calcalcal/sejong_log/Backend_Server/domain/category/common/CategoryType.java
+++ src/main/java/com/calcalcal/sejong_log/Backend_Server/domain/category/common/CategoryType.java
@@ -1,5 +1,5 @@
 package com.calcalcal.sejong_log.Backend_Server.domain.category.common;
 
 public enum CategoryType {
-    ACADEMIC, COLLEGE, DEPARTMENT, DO_DREAM, CLUB, NONE
+    ACADEMIC, COLLEGE, DEPARTMENT, DO_DREAM, CLUB, PERSONAL
 }
